985eb35ad82f6d7bad173de32cbd15f38878c4b3
yejin5 <ohyejin1117@gmail.com>
2024-03-21 13:45:03 +0900

    add lombok

--- src/main/java/com/restapi/simpleapi/controller/QuickController.java
+++ src/main/java/com/restapi/simpleapi/controller/QuickController.java
@@ -1,13 +1,16 @@
 package com.restapi.simpleapi.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class QuickController {
 
     @GetMapping("/dummy")
     public String dummy() {
+        log.info("dummy log");
         return "{}";
     }
 
