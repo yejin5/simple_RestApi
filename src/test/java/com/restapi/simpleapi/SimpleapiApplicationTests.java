1d6bf7c361637de9c0e47c25d1c63430b994822a
yejin5 <ohyejin1117@gmail.com>
2024-03-21 13:25:32 +0900

    Initial commit

---
+++ src/test/java/com/restapi/simpleapi/SimpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
